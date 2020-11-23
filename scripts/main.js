e9fc9ed3fe415d1ef895c450cb8cd21070393ac7
MegSemrad <mlsemrad753@gmail.com>
2020-11-23 10:13:03 -0600

    exercise 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,17 @@
-console.log("Welcome to the main module")
+console.log("Welcome to the main module");
 
 
 // Exercise #1 - Nov 19, 2020
-// Functions can return a value that we can capture:
-// Write a function that takes a string of a dog breed as an argument (like 'border collie')
-// Have the function return "my favorite dog breed is" plus the passed in string. If no argument is passed to the function, return "I like cats".
-// Execute the function in a way that captures the returned value in a variable.
-// Console.log the string "When it comes to pets," plus the returned value of the function.
+/* Functions can return a value that we can capture:
+Write a function that takes a string of a dog breed as an argument (like 'border collie')
+Have the function return "my favorite dog breed is" plus the passed in string. If no argument is passed to the function, return "I like cats".
+Execute the function in a way that captures the returned value in a variable.
+Console.log the string "When it comes to pets," plus the returned value of the function.
+*/
 
 const favoritePet = (petString) => {
     if (petString) {
-        return `My favorite dog breed is ${petString}`
+        return `my favorite dog breed is ${petString}`
     }
     else {
         return "I like cats"
@@ -18,14 +19,29 @@ const favoritePet = (petString) => {
 
 }
 
-const preferDog = favoritePet("rottweiler")
-const preferCat = favoritePet()
+const preferDog = favoritePet("rottweiler");
+const preferCat = favoritePet();
 
-console.log("When it comes to pets," + ${preferDog});
-console.log("When it comes to pets," + ${preferCat});
-
-
+console.log("When it comes to pets, " + preferDog);
+console.log("When it comes to pets, " + preferCat);
 
 
 
-// Exercise #2
+
+
+// Exercise #2 - Nov 23, 2020
+/*
+  1. Add an action that the painter can perform ( a property whose value is a function ).
+  2. Have her take that action by execucting the method you created.
+  */
+
+let painter = {
+    tools: ["brush", "roller", "caulk", "sandpaper", "dropcloth"],
+    uniform: "white overalls",
+    cost_per_hour: 25.00,
+    insured: true, 
+    sendInvoice: () => "faxed"
+  };
+
+  console.log('function', painter.sendInvoice());
+  
