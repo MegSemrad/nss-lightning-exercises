0c971ed8b5e75d8b1e0cd677509772b37821e313
MegSemrad <mlsemrad753@gmail.com>
2020-12-06 11:57:45 -0600

    exercise 4 with notes

---
+++ scripts/family.js
@@ -0,0 +1,32 @@
+export const useFamily = () => {
+    return family.slice()
+};
+
+
+let family = [
+    {
+      name: "Fred Jones",
+      age: 49,
+      title: "parent"
+    },
+    {
+      name: "Pat Jones",
+      age: 50,
+      title: "parent"
+    },
+    {
+      name: "Bubba Jones",
+      age: 20,
+      title: "adult child"
+    },
+    {
+      name: "Kelly Jones",
+      age: 12,
+      title: "dependent child"
+    },
+    {
+      name: "Bartleby",
+      age: 3,
+      title: "pet"
+    }
+  ]
--- scripts/main.js
+++ scripts/main.js
@@ -105,3 +105,161 @@ const button = document.querySelector(".btn");
 button.addEventListener("click", () => {
     console.log("btn was clicked")
 });
+
+
+
+
+
+/*Exercise #4
+
+1. Add a <section> element with an id of family to your index.html
+2. Use a good old for loop to iterate over this array of objects to pull out the names of the 
+   family members and represent them as HTML by wrapping each in an <h3> tag. Append them to the DOM.
+3. Achieve the same result using .map()
+ */
+
+
+/* Instructor Examples
+const familyEl = document.querySelector("#family")
+let family = [
+  {
+    name: "Fred Jones",
+    age: 49,
+    title: "parent"
+  },
+  {
+    name: "Pat Jones",
+    age: 50,
+    title: "parent"
+  },
+  {
+    name: "Bubba Jones",
+    age: 20,
+    title: "adult child"
+  },
+  {
+    name: "Kelly Jones",
+    age: 12,
+    title: "dependent child"
+  },
+  {
+    name: "Bartleby",
+    age: 3,
+    title: "pet"
+  }
+]
+
+const familyNames = []
+
+for ( let i=0; i < family.length; i++) {
+  familyNames.push(`<h3>${family[i].name}</h3>`)
+}
+familyEl.innerHTML += familyNames.join("")
+*/
+// ----------------------------------------------------------------
+
+
+
+// ----------------------------------------------------------------
+
+
+// Mine - DID NOT FINISH IN TIME - is not right
+/*  import { useFamily } from './family.js'
+ const contentTarget = document.querySelector("#family");
+
+const familyNames = () => {
+  const familyNamesArray = useFamily();
+    for (name of familyNamesArray) {
+      name.map(familyObject => {
+        const selectedName = familyObject.name
+        return `<h3>${selectedName}</h3>`
+      })
+    }
+  }
+}
+familyNames(family);
+*/
+
+// ----------------------------------------------------------------
+/*
+If want to look over entire Array and select certain one according to certain criteria
+*/
+let family = [
+  {
+    name: "Fred Jones",
+    age: 49,
+    title: "parent"
+  },
+  {
+    name: "Pat Jones",
+    age: 50,
+    title: "parent"
+  },
+  {
+    name: "Bubba Jones",
+    age: 20,
+    title: "adult child"
+  },
+  {
+    name: "Kelly Jones",
+    age: 12,
+    title: "dependent child"
+  },
+  {
+    name: "Bartleby",
+    age: 3,
+    title: "pet"
+  }
+]
+// Takes an argument of a function
+// Beneath condition that must be met for familyMember to be added to new Array called adults
+
+const adults = family.filter( (familyMember) => {
+  if (familyMember.age >= 18) {
+    return true 
+  }
+} ) 
+//  Can use below line as alternative if do not want to use if statement 
+// const adults = family.filter( (familyMember) => familyMember.age >= 18)
+// Could also put .map() on the end of the above line and put it into DOM
+// or could simply put adults.map()
+
+console.log('grownUp?', adults);
+
+
+//  ------------------------------------
+
+// .find - only finds single thing and creates an array with it inside 
+// parent below is a newly created array
+// If will find the first thing in the family array that meets that requirement and returns it then stops 
+
+let family = [
+  {
+    name: "Fred Jones",
+    age: 49,
+    title: "parent"
+  },
+  {
+    name: "Pat Jones",
+    age: 50,
+    title: "parent"
+  },
+  {
+    name: "Bubba Jones",
+    age: 20,
+    title: "adult child"
+  },
+  {
+    name: "Kelly Jones",
+    age: 12,
+    title: "dependent child"
+  },
+  {
+    name: "Bartleby",
+    age: 3,
+    title: "pet"
+  }
+]
+
+const parent = family.find( (familyMember) => familyMember.title === "parent")
+console.log("parent", parent);
